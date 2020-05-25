9fd1d0d0778d21ef13342f16e8f91d2f6a1f9175
Rusty Mina <rustymina@gmail.com>
2020-05-25 22:45:43 +0900

    Finished Adult dataset

--- adult.py
+++ adult.py
@@ -1,9 +1,17 @@
 # Adult dataset
 # Preprocessing is largely based on https://github.com/eth-sri/lcifr/blob/master/code/datasets/adult.py
 # 
-# Data                  : 112 Dimensions ( / {Sex, Income})
-# Sensitive attribute   :   1 Dimension  (Sex)
-# Ground Truth          :   1 Dimension  (Income)
+#   Features              : 102 Dimensions ( / {Sex, Income})
+#   Sensitive attribute   :   1 Dimension  (Sex)
+#   Ground Truth Label    :   1 Dimension  (Income)
+#
+#   Please note that the features does not contain the sensitive attribute. 
+#   This is in contrast to the assumptions of Ruoss et. al https://github.com/eth-sri/lcifr/issues/1
+#   which are by the way based on Data Producer - Data Consumer Framework of McNamara et. al
+#
+#   We highly suggest that you concatenate the features and sensitive instead in cases where 
+#   Data Producer is assumed to have access to every feature, including the sensitive attribute
+
 
 import os
 import urllib.request as ur
@@ -29,7 +37,7 @@ column_names = [
 ]
 
 # Sensitive Attribute
-sensitive_attribute = "sex"
+sensitive_attribute = 'sex'
 
 def _download_one( filename ):
     """
@@ -50,7 +58,7 @@ def _download_one( filename ):
         print( "Found and verified ", filepath )
 
 def _adult_download():
-    for ket in adult_filenames:
+    for key in adult_filenames:
         _download_one(adult_filenames[key])
 
 def _read_data_file(path, device, train=True, normalize=True):
@@ -67,11 +75,16 @@ def _read_data_file(path, device, train=True, normalize=True):
     data_frame.dropna(axis=0, inplace=True)                                                 # Drop missing values
 
     # Encode labels
-    label_map = {'<=50K': 0, '>=50K': 1}
+    label_map = ( {'<=50K': 0, '>50K': 1} if train else {'<=50K.': 0, '>50K.': 1} )
     data_frame.replace(label_map, inplace=True)                                             # Replace labels from string to Binary
 
     # Split Features and labels
-    features, labels = data_frame.drop('income', axis=1), data_frame['income']
+    features, labels, sensitive = data_frame.drop(['income', sensitive_attribute], axis=1), data_frame['income'], data_frame[sensitive_attribute]
+
+    # Preprocess sensitive attribute
+    # Do this before converting categorical to one-hot though pd.get_dummies
+    # Otherwise you will encounter a KeyError, because the 'sex' column is already gone -> 'sex0', 'sex1'
+    a = np.logical_not( pd.Categorical( sensitive ).codes )             # pd.Categorical().codes converts categorical strings to categorical integer values
 
     # Categorize where a column is a (1) continuous variable or a (2) categorical variable
     # We need to store the column names because their index will change once we convert categorical variables to one-hot encodings
@@ -81,21 +94,22 @@ def _read_data_file(path, device, train=True, normalize=True):
         if (features[col].isnull().sum() > 0):
             features.drop(col, axis=1, inplace=True)
         else:
-            if (features[col].type == np.object):
+            if (features[col].dtype == np.object):
                 categorical_columns += [col]
             else:
                 continuous_vars += [col]
 
     # Transform Categorical Varables into their One-hot encoding
-    features = pd.get_dummies(features, columns=categorical_colums, prefix_sep='=')
+    features = pd.get_dummies(features, columns=categorical_columns, prefix_sep='=')
     continuous_columns = [features.columns.get_loc(var) for var in continuous_vars]
 
     # Add missing columns to the test dataset
     # After applying pd.get_dummies(), train_features returned 104 columns 
     # while test_features returned 103 columns
-    if (train==False):
+    if (train == False):
         features.insert(
-            loc = features.clumns.get_loc('native_country=Holand-Netherlands'),
+            # loc = features.columns.get_loc('native_country=Holand-Netherlands'),
+            loc = 75,
             column = 'native_country=Holand-Netherlands', value=0
         )
     
@@ -104,17 +118,14 @@ def _read_data_file(path, device, train=True, normalize=True):
     # Notice the 'sex' column, which is also our sensitive variable, has only 2 unique values
     one_hot_columns = {}
     for column_name in categorical_columns:
-        ids = [i for i, col in enumerate(features.columns) if col.startswith("{}=").format(column_name)]
+        ids = [i for i, col in enumerate(features.columns) if col.startswith("{}=".format(column_name))]
         if (len(ids) > 0):
             assert len(ids) == ids[-1] - ids[0] + 1
         one_hot_columns[column_name] = ids
-    
+
     # Save the column name and column indexes of the final dataframe
     # column_ids = {col: idx for idx, col in enumerate(features.columns)}
-
-    # Preprocess sensitive attribute
-    a = np.logical_not( pd.Categorical( features[sensitive_attribute] ).codes )             # pd.Categorical().codes converts categorical strings to categorical integer values
-    
+        
     # Convert data to torch tensor
     x = torch.tensor( features.values.astype(np.float32), device=device )
     y = torch.tensor( labels.values.astype(np.int64), device=device )
@@ -126,7 +137,7 @@ def _read_data_file(path, device, train=True, normalize=True):
         mean, std = x.mean(dim=0)[columns], x.std(dim=0)[columns]
         x[:, columns] = (x[:, columns] - mean) / std    
 
-    return x, y, z
+    return x, y, a
 
 def load_dataset(download=True, train=True, device="cpu"):
     """
@@ -139,7 +150,9 @@ def load_dataset(download=True, train=True, device="cpu"):
 
     if (download):
         _adult_download()
-    # if (train):
-    #     features, label, sensitive = 
-    # else:
-    #     features, label, sensitive = 
+    if (train):
+        features, label, sensitive = _read_data_file( adult_download_folder + adult_filenames['train'], device=device, train=True)
+    else:
+        features, label, sensitive = _read_data_file( adult_download_folder + adult_filenames['test'], device=device, train=False)
+
+    return features, label, sensitive
