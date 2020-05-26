363dbca795392197507f5449894d39487d95754a
Rusty Mina <rustymina@gmail.com>
2020-05-27 03:52:41 +0900

    Cleaned extYaleB.py

--- extYaleB.py
+++ extYaleB.py
@@ -17,36 +17,12 @@ from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 
 DATASET_PATH = "data/"
-DATASET_FOLDER = "CroppedYale"
 
 class PGMDataset(Dataset):
 
     URL = "http://vision.ucsd.edu/extyaleb/CroppedYaleBZip/CroppedYale.zip"
     DATASET_FILE_NAME = "CroppedYale.zip"
     DATASET_FOLDER = "CroppedYale/"
-
-    def _download_one(self, url):
-        file_path = self.dataset_path
-        # extended_dataset_path = self.dataset_path + PGMDataset.DATASET_FOLDER
-        zip_path = self.dataset_path + PGMDataset.DATASET_FILE_NAME
-
-        # "data/"
-        if not os.path.exists(file_path):
-            os.makedirs( file_path )
-
-        # "data/CroppedYale.zip"    
-        if not os.path.exists( zip_path ):
-            print( "Downloading ", PGMDataset.DATASET_FILE_NAME, " ...")
-
-            with ur.urlopen(PGMDataset.URL) as response, open(zip_path, 'wb') as out_file:
-                data = response.read()
-                out_file.write(data)
-
-    def _extract_zip(self):
-        zip_path = self.dataset_path + PGMDataset.DATASET_FILE_NAME
-        print("Extracting ", zip-path, " to ", self.dataset_path)
-        with zipfile.ZipFile(zip_path, "r") as zip_ref:
-            zip_ref.extractall(self.dataset_path)
 
     def __init__(self, dataset_path="data/", download=True, transform=None):
         self.dataset_path = dataset_path
@@ -97,22 +73,46 @@ class PGMDataset(Dataset):
     def __len__(self):
         return len(self.image_paths)
 
-def test():
-    # Device
-    device = ("cuda" if torch.cuda.is_available() else "cpu")
+    def _download_one(self, url):
+        file_path = self.dataset_path
+        zip_path = self.dataset_path + PGMDataset.DATASET_FILE_NAME
 
-    # Dataset and Dataloader
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-    dataset = PGMDataset(DATASET_PATH, download=True, transform=transform)
-    BATCH_SIZE = 1
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+        # "data/"
+        if not os.path.exists(file_path):
+            os.makedirs( file_path )
 
-    aye = []
-    for images, labels in dataloader:
-        # curr_batch_size = images.shape[0]
-        aye.append(str(images.shape))
+        # "data/CroppedYale.zip"    
+        if not os.path.exists( zip_path ):
+            print( "Downloading ", PGMDataset.DATASET_FILE_NAME, " ...")
+
+            with ur.urlopen(PGMDataset.URL) as response, open(zip_path, 'wb') as out_file:
+                data = response.read()
+                out_file.write(data)
+
+    def _extract_zip(self):
+        zip_path = self.dataset_path + PGMDataset.DATASET_FILE_NAME
+        print("Extracting ", zip-path, " to ", self.dataset_path)
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(self.dataset_path)
+
+# Uncomment this to debug
+# def test():
+#     # Device
+#     device = ("cuda" if torch.cuda.is_available() else "cpu")
+
+#     # Dataset and Dataloader
+#     transform = transforms.Compose([
+#         transforms.ToTensor()
+#     ])
+#     dataset = PGMDataset(DATASET_PATH, download=True, transform=transform)
+#     BATCH_SIZE = 1
+#     dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+#     aye = []
+#     for images, labels in dataloader:
+#         # curr_batch_size = images.shape[0]
+#         aye.append(str(images.shape))
         
+#     print(list(set(aye)))
 
 # test()
