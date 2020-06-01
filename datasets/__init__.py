c2a3b743db0a2e4ee0a323a3e90d560fb8ca0559
Rusty Mina <rustymina@gmail.com>
2020-06-01 18:45:36 +0900

    Moved dataset python files to datasets folder | Added TabularFairnessDataset to encapsulate tensors to a torch Dataset format

---
+++ datasets/__init__.py
@@ -0,0 +1,6 @@
+from . import adult
+from . import compas
+from . import extYaleB
+from . import german
+from . import health
+from . import utils
---
+++ datasets/utils.py
@@ -0,0 +1,18 @@
+import torch
+
+# FairnessDataset
+# This is torch.utils.data.Dataset for common fairness datasets 
+# which is composed of (X,y,a) -> (Features, Labels, Sensitive Attribute)
+class TabularFairnessDataset(torch.utils.data.Dataset):
+    def __init__(self, x, y, a):
+        assert x.shape[0] == y.shape[0], "Features and Labels are expected to have same number of instances"
+        assert y.shape[0] == a.shape[0], "Labels and Sensitive Attributes are expected to have same number of instances"
+        self.x = x
+        self.y = y
+        self.a = a
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index], self.a[index]
+    
+    def __len__(self):
+        return self.x.shape[0]
